3a9832012852d7dde31c87b5efca4fc14eca0c25
Milad Soliman <miladsoliman55@gmail.com>
2023-10-17 14:53:13 +0300

    finishing design of home screen ui

--- app/src/main/java/com/example/runprof_task/homeScreen/HomeViewModel.kt
+++ app/src/main/java/com/example/runprof_task/homeScreen/HomeViewModel.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.runprof_task.homeScreen.domain.GetPopularMoviesUseCase
+import com.example.runprof_task.homeScreen.model.Movie
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 import kotlinx.coroutines.launch
 
@@ -11,6 +14,8 @@ class HomeViewModel :ViewModel() {
 
     val useCase =  GetPopularMoviesUseCase()
 
+    private var _popularMovies = MutableStateFlow<List<Movie>>(emptyList())
+    var  popularMovies : StateFlow<List<Movie>> = _popularMovies
 
     init {
         printLogs()
@@ -21,7 +26,7 @@ class HomeViewModel :ViewModel() {
      fun getMovies() {
         viewModelScope.launch {
          val list  = useCase.execute()
-
+          _popularMovies.value = list
            Log.i("Mizooo","DAta" + list.size)
         }
     }
--- app/src/main/java/com/example/runprof_task/homeScreen/presentation/HomeFragment.kt
+++ app/src/main/java/com/example/runprof_task/homeScreen/presentation/HomeFragment.kt
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.runprof_task.R
+import com.example.runprof_task.databinding.FragmentHomeBinding
+import com.example.runprof_task.homeScreen.HomeViewModel
+import com.example.runprof_task.homeScreen.model.Movie
+import kotlinx.coroutines.launch
 
 
 class HomeFragment : Fragment() {
+    private lateinit var homeBinding: FragmentHomeBinding
+    private lateinit var homeAdapter: HomeAdapter
+    private lateinit var homeViewModel: HomeViewModel
+  //  lateinit var myMovies: List<Movie>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +31,22 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        homeBinding = FragmentHomeBinding.inflate(inflater)
+        homeAdapter = HomeAdapter(listOf())
+        homeViewModel = HomeViewModel()
+        return homeBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeBinding.homeRV.adapter = homeAdapter
+        homeBinding.homeRV.layoutManager = GridLayoutManager(requireContext(),2)
+        lifecycleScope.launch {
+            homeViewModel.popularMovies.collect{
+               homeAdapter.updateFavList(it)
+            }
+        }
     }
 
 }
